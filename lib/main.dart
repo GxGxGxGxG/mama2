8af635789804ccd96d45cf21499a1057398416f2
GxG <georgeluta6@gmail.com>
2021-07-30 13:12:21 +0300

    mama mea e florareasa

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+//import 'package:audioplayers/audioplayers.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+         
+
+
+        ),
+      ),
+    );
+  }
+}
